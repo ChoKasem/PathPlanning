a8d24c24316b3f5c6c188acbe4d4ec1663a93399
Jo <chokpisit.kasemphaibulsuk@mail.utoronto.ca>
2020-01-06 13:42:05 -0400

    Add Breath-First Search

--- testing.py
+++ testing.py
@@ -21,7 +21,7 @@ queue = []
 explore_order = [[0,1],
                  [-1,0],
                  [0,-1],
-                 [1,1]]
+                 [1,0]]
 explore_order = np.array(explore_order)
 # print(explore_order[1])
 # print(world[explore_order[1][0]][explore_order[1][1]])
@@ -30,15 +30,18 @@ explore_order = np.array(explore_order)
 # #Breath First Search
 count = 1
 
-exploring = start
-while exploring != goal:
+exploring = np.array(start)
+while not all(exploring == goal):
     for i in explore_order:
         explore_next = exploring + i
-        if explore_next[0] >=0 and explore_next[0] < total_row and explore_next[1] >=0 and explore_next[1] < total_col and world[explore_next[0]][explore_next][1] >= 0:
+        if explore_next[0] >=0 and explore_next[0] < total_row and explore_next[1] >=0 and explore_next[1] < total_col and world[explore_next[0]][explore_next][1] == 0 and not all(explore_next == start):
             queue.append(explore_next)
             world[explore_next[0]][explore_next[1]] = count
             count+=1
-    exploring = queue[0]
-    queue.pop(0)
-    print(world)
+    if not queue:
+        print("No Solution")
+        break
+    else:
+        exploring = queue[0]
+        queue.pop(0)
 
